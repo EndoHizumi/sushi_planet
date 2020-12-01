33fd2cb9390e01cdc520edc79efb6ac96e7f36e8
Endo_Hizumi <endohizumi0114@gmail.com>
2020-12-01 11:09:31 +0900

    Added function to increase / decrease sushi satelitte

--- src/components/planet.vue
+++ src/components/planet.vue
@@ -1,25 +1,47 @@
 <template>
-      <div id="root">
-      <div id="planet">
+  <div id="root">
+    <div id="planet">
       <img v-bind:class="state" v-bind:src="url" />
     </div>
+    <Satelitte
+      v-for="num of satellite_nums"
+      v-bind:key="num"
+      v-bind:ref="'satelitte'"
+    ></Satelitte>
+  </div>
 </template>
 
 <script>
+import Satelitte from "./satelitte";
 export default {
+  components: { Satelitte },
   props: {
     url: String,
     state: String,
   },
-    methods:{
-        add:function(){
-            console.log('planet add')
-        },
-        reduce: function(){
-            console.log('planet_reduce')
-        }
-    }
-}
+  data: function () {
+    return {
+      satellite_nums: 1,
+    };
+  },
+  methods: {
+    add: function () {
+      let current_satelitte = this.$refs['satelitte'][this.satellite_nums -1]
+      if (current_satelitte.get_count() > 2) {
+        this.satellite_nums++
+      }
+      current_satelitte.add();
+    },
+    reduce: function () {
+      let current_satelitte = this.$refs['satelitte'][this.satellite_nums -1]
+      if (current_satelitte.get_count() > 0) {
+        this.satellite_nums--
+        current_satelitte = this.$refs['satelitte'][this.satellite_nums -1]
+      }
+      current_satelitte.reduce();
+    },
+  },
+};
 </script>
 
 <style>
---
+++ src/components/satelitte.vue
@@ -0,0 +1,74 @@
+<template>
+  <div id="satellite" class="spin">
+    <div v-for="food in foods" v-bind:key="food.index">
+      <span v-bind:class="position_list[food.index]">{{food.item}}</span>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  components: {
+  },
+  data: function () {
+    return {
+      count: 0,
+      foods: [],
+      position_list: ["left_up", "right_up", "left_bottom", "right_bottom"],
+    };
+  },
+  methods: {
+    get_count: function(){
+      return this.count
+    },
+    add: function () {
+      var food_num = this.foods.length;
+      this.foods.push({ index: food_num, item: "🍣" });
+      this.count++
+    },
+    reduce: function () {
+      if (this.foods.length > 0) {
+        this.foods.pop();
+        this.count--
+      }
+    },
+  },
+};
+</script>
+
+<style>
+.left_up{
+  position: absolute;
+  left: 0;
+}
+
+.right_up{
+  position: absolute;
+  right: 0;
+}
+
+.left_bottom{
+  position: absolute;
+  left: 0;
+  bottom: 0;
+}
+
+.right_bottom{
+  position: absolute;
+  right: 0;
+  bottom: 0;
+}
+
+.spin {
+  animation: spin 1.5s linear infinite;
+}
+
+@keyframes spin {
+  0% {
+    transform: rotate(0deg);
+  }
+  100% {
+    transform: rotate(360deg);
+  }
+}
+</style>
